28ef0745a9ba8859f94f2e39a8570d8cdaa5795b
MD: Miel Mahmud  Sifat <125751571+milu892@users.noreply.github.com>
2023-11-01 20:33:32 +0600

    Add files via upload

---
+++ function Hello .c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void printHello();
+void printGoodBye();
+int main(){
+
+printHello();
+printGoodBye();
+
+
+//write 2 functions-one to print "hello"&Second to print "GoodBye".
+
+
+}
+void printHello(){
+printf("Hello\n");
+
+}
+void printGoodbye(){
+printf("GoodBye");
+
+}
